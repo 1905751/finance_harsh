f55a624ee14f7f46f422cc6d92c4e55aebe14eab
1905751 <1905751@users.noreply.github.com>
2020-04-14 15:01:30 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- credit.c
+++ credit.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <cs50.h>
 
+long luhn(long,long);
+
 int main(void)
 {
-    int len=0,x=0;
-    long n = get_long("Number: ");
+    long len=0,x=0,c,n;
+    long number = get_long("Number: ");
+    n=number;
 
     while (n != 0)
     {
@@ -12,24 +15,40 @@ int main(void)
         len++;
         if (len == 13 &&(n == 34||n == 37))
         {
-            printf("AMEX\n");
-            x++;
+            c=luhn(15,number);
+            if(c%10==0)
+            {
+                printf("AMEX\n");
+                x++;
+            }
         }
         if ( len == 12 && n == 4)
         {
-            printf("VISA\n");
-            x++;
+            c=luhn(13,number);
+            if(c%10==0)
+            {
+                printf("VISA\n");
+                x++;
+            }
         }
         if(len==14&&(n>=51&&n<=55))
         {
-            printf("MASTERCARD\n");
-            x++;
+            c=luhn(16,number);
+            if(c%10==0)
+            {
+                printf("MASTERCARD\n");
+                x++;
+            }
         }
-       /* if(len==15&&n==4)
+        if(len==15&&n==4)
         {
-            printf("VISA\n");
-            x++;
-        }*/
+            c=luhn(16,number);
+            if(c%10==0)
+            {
+                printf("VISA\n");
+                x++;
+            }
+        }
     }
 
     if(x==0)
@@ -38,3 +57,34 @@ int main(void)
     }
 
 }
+
+
+long luhn(long b,long num)
+{
+    long i,s=0,y=0;
+    for(i=0;i<b;i++)
+    {
+        if(i%2==0)
+        {
+            s=s+num%10;
+            num=num/10;
+        }
+        else
+        {
+            if(2*(num%10)>9)
+            {
+                y=2*(num%10);
+                s=s+(y%10)+(y/10);
+                num=num/10;
+            }
+            else
+            {
+                s=s+2*(num%10);
+                num=num/10;
+            }
+        }
+    }
+    return s;
+}
+
+
