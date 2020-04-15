e469b75ea0f92fbd17515bbac1e382b834b9aeb7
1905751 <1905751@users.noreply.github.com>
2020-04-15 20:00:34 +0000

    automated commit by check50 [check50=True]

--- substitution.c
+++ substitution.c
@@ -4,81 +4,88 @@
 
 int rev(string key);
 
+// main programme
 int main(int argc, string argv[])
 {
 
     if (argc == 2)
     {
+        //variables
         int x, i, z = 0, y;
         string str, pt;
-       str = argv[1];
-       for (i = 0; str[i] != '\0'; i++)
-       {
-           z++;
-       }
-       if(z != 26)
-       {
-           printf("Key must contain 26 characters.");
-           return 1;
-       }
+        str = argv[1];    //copying argv to str
+        //loop to check length of sting
+        for (i = 0; str[i] != '\0'; i++)
+        {
+            z++;
+        }
+        if (z != 26)
+        {
+            printf("Key must contain 26 characters.");
+            return 1;
+        }
        
-       // to do checking of validity of key
-       y = rev(str);
-       if(y == 1)
-       {
-           printf("invalid");
-           return 1;
-       }
+        //checking of validity of key
+        //calling rev
+        y = rev(str);
+        if (y == 1)
+        {
+            printf("invalid");
+            return 1;
+        }
 
 
-       pt = get_string("plaintext: ");
-       printf("ciphertext: ");
+        //taking user input
+        pt = get_string("plaintext: ");
+        printf("ciphertext: ");
 
-       for (i = 0; pt[i] != '\0'; i++)
-       {
-           x = (int) pt[i];
-           if (x >= 65 && x <= 90)
-           {
-               x = x - 65;
-               if (str[x] >= 65 && str[x] <= 90)
-               {
-                   printf("%c",str[x]);
-               }
-               else
-               {
-                   printf("%c",str[x] - 32);
-               }
-           }
-           else if (x >= 97 && x <= 122)
-           {
-               x = x - 97;
-               if (str[x] >= 97 && str[x] <= 122)
-               {
-                   printf("%c",str[x]);
-               }
-               else
-               {
-                   printf("%c",str[x] + 32);
-               }
-           }
-           else
-           {
-               printf("%c", pt[i]);
-           }
+        //loop to cipher the plaintext
+        for (i = 0; pt[i] != '\0'; i++)
+        {
+            x = (int) pt[i];
+            if (x >= 65 && x <= 90)
+            {
+                x = x - 65;
+                if (str[x] >= 65 && str[x] <= 90)
+                {
+                    printf("%c", str[x]);
+                }
+                else
+                {
+                    printf("%c", str[x] - 32);
+                }
+            }                                                    //conditions to allign key according to plain test
+            else if (x >= 97 && x <= 122)
+            {
+                x = x - 97;
+                if (str[x] >= 97 && str[x] <= 122)
+                {
+                    printf("%c", str[x]);
+                }
+                else
+                {
+                    printf("%c", str[x] + 32);
+                }
+            }
+            else
+            {
+                printf("%c", pt[i]);                            //to print characters rather than alphabets
+            }
            
 
-       }
-       printf("\n");
-       return 0;
+        }
+        printf("\n");
+        return 0;
 
     }
-        printf("no key");
-        return 1;
+    printf("no key");         //no key condition
+    return 1;
 }
 
 
 
 
+//function to check repetetion or key validity
 int rev(string key)
 {
     int i, j, k;
@@ -86,16 +93,22 @@ int rev(string key)
     {
         k = (int) key[i];
         if (k >= 65 && k <= 90)
-        k = k - 65;
-        else if (k >= 97 && k <= 122)
-        k = k - 97;
-        else
-        return 1;
-        
-        
-        for (j = i+1; key[j] != '\0'; j++)
         {
-            if(key[i] == key[j])
+            k = k - 65;
+        }
+        else if (k >= 97 && k <= 122)         //for key validity
+        {
+            k = k - 97;
+        }
+        else
+        {
+            return 1;
+        }
+        
+        
+        for (j = i + 1; key[j] != '\0'; j++)
+        {
+            if (key[i] == key[j])           //for repetetion
             {
                 return 1;
             }
