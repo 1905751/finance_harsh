848e89dac4b44da5d70d93ddd1250b88b287db56
1905751 <1905751@users.noreply.github.com>
2020-04-29 15:38:14 +0000

    automated commit by check50 [check50=True]

--- dictionary.c
+++ dictionary.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -24,7 +25,29 @@ node *table[HASHTABLE_SIZE];
 // Returns true if word is in dictionary else false
 bool check(const char *word)
 {
-    // TODO
+    // initialise lower case word
+    char lcword[LENGTH+1];
+
+    // convert to lowercase, as we need this to lookup. TODO: change from length to length of word
+    for (int i = 0; i < LENGTH; i++)
+    {
+        lcword[i] = tolower(word[i]);
+    }
+
+    // set cursor to start of appropriate lined list
+    node *cursor = table[hash(lcword)];
+
+
+    // traverse list
+    while (cursor != NULL)
+    {
+        // check node's word to see if it is target word, ignoring case
+        if (strcasecmp(word, cursor->word) == 0)
+        {
+            return true;
+        }
+        cursor = cursor->next;
+    }
     return false;
 }
 
@@ -33,8 +56,10 @@ unsigned int hash(const char *word)
 {
     // TODO
     unsigned int hash = 0;
-    for (int i=0, n=strlen(word); i<n; i++)
+    for (int i = 0, n = strlen(word); i < n; i++)
+    {
         hash = (hash << 2) ^ word[i];
+    }
     return hash % HASHTABLE_SIZE;
 }
 
@@ -75,13 +100,40 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
-    return 0;
+    int counter = 0;
+
+    // iterate through hashtable
+    for (int i = 0; i < HASHTABLE_SIZE; i++)
+    {
+        // set pointer to head of list
+        node *cursor = table[i];
+
+        // traverse list
+        while (cursor != NULL)
+        {
+            counter++;
+            cursor = cursor->next;
+        }
+    }
+    return counter;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void)
 {
-    // TODO
-    return false;
+     for (int i = 0; i < HASHTABLE_SIZE; i++)
+    {
+        // set pointer to head of list
+        node *cursor = table[i];
+
+        // traverse list
+        while (cursor != NULL)
+        {
+            node *temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+        }
+        free(cursor);
+    }
+    return true;
 }
