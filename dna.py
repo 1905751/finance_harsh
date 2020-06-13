39185897ff186b561d0cc65ae33eb028f91c1761
1905751 <1905751@users.noreply.github.com>
2020-06-13 16:59:18 +0000

    automated commit by submit50

--- dna.py
+++ dna.py
@@ -18,11 +18,43 @@ with open(argv[2]) as dna_file:
 
     for line in read:
         seq  = line[0]
+i = 0
+while i < len(key):
+    counter = 0
+    big = 0
+    j = 0
 
-x1 = 0
-for i in range(len(key)):
-    x = len(key[i])
-    for j in range(len(seq)):
-        if sub[j : j + x ] == key[i]:
-            a = a + 1
-        
+    while j < len(seq):
+        sub = seq[j : j + len(key[i])]
+        if sub == key[i]:
+            counter += 1
+            j += len(key[i])
+        else:
+            if counter > big:
+                big = counter
+            if counter != 0:
+                counter = 0
+            j += 1
+
+    key[i] = big
+    i += 1
+
+with open(argv[1]) as people_info:
+    people = csv.reader(people_info)
+
+    next(people)
+    nm = 0
+    for p in people:
+        i = 1
+        match = 0
+        while i < (len(p)):
+            if int(p[i]) == key[i - 1]:
+                match += 1
+
+            i += 1
+        if match == len(key):
+                nm += 1
+                print(p[0])
+                break
+if nm == 0:
+    print("no match")
